61169a067717bbd4dfe7642076df42ef273fe176
chiashengchen <peterchen40213@yahoo.com.tw>
2023-06-29 21:20:22 +0800

    商品功能 - 實作查詢商品列表 (基礎)

--- demo/src/main/java/com/example/demo/dao/ProductDao.java
+++ demo/src/main/java/com/example/demo/dao/ProductDao.java
@@ -3,10 +3,14 @@ package com.example.demo.dao;
 import com.example.demo.dto.ProductRequest;
 import com.example.demo.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
     public Product getProductById(Integer productId);
     public Integer createProduct(ProductRequest request);
     public void updateProduct(Integer productId, ProductRequest request);
     public void deleteProductById(Integer productId);
+
+    List<Product> getProducts();
 }
--- demo/src/main/java/com/example/demo/service/ProductService.java
+++ demo/src/main/java/com/example/demo/service/ProductService.java
@@ -3,9 +3,13 @@ package com.example.demo.service;
 import com.example.demo.dto.ProductRequest;
 import com.example.demo.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     public Product getProductById(Integer productId);
     public Integer createProduct(ProductRequest request);
     public void updateProduct(Integer productId, ProductRequest request);
     public void deleteProductById(Integer productId);
+
+    List<Product> getProducts();
 }
